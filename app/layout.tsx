d64e7874c564d734914721b12421cf84af31a96f
pyraxo <pyraxo@pyraxo.moe>
2024-10-06 18:15:47 +0800

    Add robots.txt, SEO keywords and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,35 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
-  title: "SUTDents",
-  description: "Quick links for SUTD students",
-};
+  title: 'SUTDents',
+  description: 'Bulletin board of quick links, useful for SUTD students.',
+  keywords: 'SUTD, quicklinks, useful',
+  openGraph: {
+    title: 'SUTDents',
+    description: 'Bulletin board of quick links, useful for SUTD students.',
+    url: 'https://sutdents.com',
+    siteName: 'SUTDents',
+    // images: [
+    //   {
+    //     url: 'https://sutdents.com/favicon.ico',
+    //     width: 1200,
+    //     height: 630,
+    //     alt: 'AI Chat History Bulletin Board',
+    //   },
+    // ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary',
+    title: 'SUTDents',
+    description: 'Bulletin board of quick links, useful for SUTD students.',
+    images: ['https://sutdents.com/favicon.ico'],
+  },
+}
 
 export default function RootLayout({
   children,
@@ -17,6 +41,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider attribute="class">
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
